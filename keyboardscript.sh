3f9a66e657598c477e7c36a542941e0a7a820dda
adithya-unni-26 <81636091+adithya-unni-26@users.noreply.github.com>
2021-08-16 15:31:13 +0530

    Update keyboardscript.sh

--- keyboardscript.sh
+++ keyboardscript.sh
@@ -1,3 +1,6 @@
+#a bash script to change change keys because certain keys don't work on my keyboard :p
+#the keyboard keycodes are obtained using xev
+
 xmodmap -e "keycode 71 = percent"
 xmodmap -e "keycode 68 = numbersign"
 xmodmap -e "keycode 70 = dollar"
