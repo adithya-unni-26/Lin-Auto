1ced2b1257c1d6cf07fb2c951bffb6f435362888
adithya-unni-26 <81636091+adithya-unni-26@users.noreply.github.com>
2021-08-16 15:28:49 +0530

    Add files via upload

---
+++ keyboardscript.sh
@@ -0,0 +1,7 @@
+xmodmap -e "keycode 71 = percent"
+xmodmap -e "keycode 68 = numbersign"
+xmodmap -e "keycode 70 = dollar"
+xmodmap -e "keycode 73 = parenleft"
+xmodmap -e "keycode 74 = parenright"
+xmodmap -e "keycode 76 = underscore"
+xmodmap -e "keycode 96 = equal" 
